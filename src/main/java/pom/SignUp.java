2e21ae3080fc10043f5dc885110258d58355a33e
PallaviNPustake <pallavinpustake@gmail.com>
2023-08-20 09:46:27 +0530

    Added signup page

---
+++ src/main/java/pom/SignUp.java
@@ -0,0 +1,13 @@
+package pom;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SignUp {
+ @Test
+ public void test()
+ {
+	Reporter.log("Sign up page", true);
+ }
+}
