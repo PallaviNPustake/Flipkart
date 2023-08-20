d622c34bf34b83329b5c0968f34ced0e3a08c76a
PallaviNPustake <pallavinpustake@gmail.com>
2023-08-20 13:36:35 +0530

    adding new branch

---
+++ src/main/java/pom/NewBranch.java
@@ -0,0 +1,13 @@
+package pom;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class NewBranch {
+
+	 @Test
+	 public void test()
+	 {
+		Reporter.log("New branch", true);
+	 }
+}
